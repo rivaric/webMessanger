7810ffd8d0a093e4069bd720995d097630fed96c
Rivaric <helium@internet.ru>
2023-09-25 18:15:41 +0300

    create register page

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -15,14 +15,36 @@ import './Form.scss';
 
 export function Form({ inputs, title, buttonText, info, picture }: FormProps) {
   return (
-    <div className="window-login">
-      <img src={picture} alt="" />
-      <div className="form-wrapper">
-        <div className="title">
+    <div
+      className="window-login"
+      style={{
+        flexDirection: `${inputs[0] == 'Username' ? 'row-reverse' : 'row'}`
+      }}>
+      <img
+        src={picture}
+        alt="picture"
+        style={{
+          transform: `translateX(${inputs[0] == 'Username' ? '-50px' : {}})`
+        }}
+      />
+      <div
+        className="form-wrapper"
+        style={{
+          paddingTop: `${inputs[0] == 'Username' ? '45px' : {}}`
+        }}>
+        <div
+          className="title"
+          style={{
+            marginBottom: `${inputs[0] == 'Username' ? '20px' : {}}`
+          }}>
           <div className="logo-img"></div>
           <h1 className="logo-text">{title}</h1>
         </div>
-        <form>
+        <form
+          style={{
+            gap: `${inputs[0] == 'Username' ? '10px' : {}}`,
+            marginBottom: `${inputs[0] == 'Username' ? '30px' : {}}`
+          }}>
           {inputs.map((name, i) => (
             <label className="label" htmlFor={name} key={i}>
               {name}
@@ -30,7 +52,13 @@ export function Form({ inputs, title, buttonText, info, picture }: FormProps) {
             </label>
           ))}
         </form>
-        <button className="form-button">{buttonText}</button>
+        <button
+          className="form-button"
+          style={{
+            marginBottom: `${inputs[0] == 'Username' ? '30px' : {}}`
+          }}>
+          {buttonText}
+        </button>
         <ul className="info">
           {info.map(({ question, link }, i) => (
             <li key={i}>
--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,3 +1,17 @@
+import { Form } from '../../components/form/Form';
+import picture from './img/Picture.png';
+
 export function Register() {
-  return <h1>Register</h1>;
+  return (
+    <Form
+      inputs={['Username', 'Email', 'Password', 'Confirm password']}
+      title="Create new account"
+      buttonText="Sign Up"
+      info={[
+        { question: 'Do you already have an account?', link: 'Sign In' },
+        { question: 'Do you have any questions?', link: 'Support' }
+      ]}
+      picture={picture}
+    />
+  );
 }
